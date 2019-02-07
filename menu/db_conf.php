62b76728816a3547c7e9080bb2c731d8a5c040c0
fred.vunk <fred.vunk@khk.ee>
2019-02-07 13:23:36 +0200

    uuendus

--- menu/db_conf.php
+++ menu/db_conf.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_USER', 'fredvunk_edgar');
+define('DB_PASS', 'uusphptest12');
+define('DB_NAME', 'fredvunk_PVK2');
 ?> 

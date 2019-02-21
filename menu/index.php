f1ed16613b230a1ed7a99697addfbe9724ebd5de
fred.vunk <fred.vunk@khk.ee>
2019-02-21 09:11:39 +0200

    Menu - lisatud andmete lugemine massiivist

--- menu/index.php
+++ menu/index.php
@@ -12,16 +12,46 @@ $contentTmpl = new Template('content');
 $courseCardTmpl = new Template('course_card');
 $courseCardHeaderTmpl = new Template('course_card_header');
 $courseCardDataTmpl = new Template('course_card_data');
-$courseNames = array(
-    'praed' => 'fa-utensils',
-    'supid' => 'fa-utensil-spoon',
-    'magustoidud' => 'fa-cookie-bite',
-    'joogid' => 'fa-glass-whiskey');
-foreach ($courseNames as $courseName => $courseIcon){
-    $courseCardHeaderTmpl->set('course_name', $courseName);
-    $courseCardHeaderTmpl->set('course_icon', $courseIcon);
+$courseDatalistTmpl = new Template('course_data_list');
+
+$courses = array(
+    array(
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
+                'dish_description' => 'sealihapada, lisand, salat, leib',
+                'dish_price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'dish_name' => 'Praetud kanakints',
+                'dish_description' => 'praetud kana, lisand, salat, leib',
+                'dish_price' => 2.50,
+                'discount' => 2.13
+            )
+        )
+    ),
+    array(
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'dish_name' => 'Rassolnik',
+                'dish_description' => 'supp, hapukoor, leib',
+                'dish_price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
+);
+foreach ($courses as $course => $courseData){
+    $courseCardHeaderTmpl->set('course_name', $courseData['name']);
+    $courseCardHeaderTmpl->set('course_icon', $courseData['icon']);
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
-    $courseCardDataTmpl->set('course_name', $courseName);
+    $courseCardDataTmpl->set('course_name', $courseData['name']);
+    $courseCardDataTmpl->set('course_data_list', $courseDatalistTmpl->parse());
     $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
     $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }

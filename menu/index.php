14042d9e5f5eebd523472862150b553cf2b3b0d1
fred.vunk <fred.vunk@khk.ee>
2019-02-21 09:29:04 +0200

    Menu - andmed loetakse massiivist ja listakse menüü malli sisse

--- menu/index.php
+++ menu/index.php
@@ -9,10 +9,6 @@ echo '</pre>';*/
 $mainTmpl = new Template('main');
 $mainTmpl->set('title', 'Menu Application');
 $contentTmpl = new Template('content');
-$courseCardTmpl = new Template('course_card');
-$courseCardHeaderTmpl = new Template('course_card_header');
-$courseCardDataTmpl = new Template('course_card_data');
-$courseDatalistTmpl = new Template('course_data_list');
 
 $courses = array(
     array(
@@ -47,12 +43,33 @@ $courses = array(
     )
 );
 foreach ($courses as $course => $courseData){
+
+    $courseCardTmpl = new Template('course_card');
+    $courseCardHeaderTmpl = new Template('course_card_header');
+    $courseCardDataTmpl = new Template('course_card_data');
+
+
+
+
     $courseCardHeaderTmpl->set('course_name', $courseData['name']);
     $courseCardHeaderTmpl->set('course_icon', $courseData['icon']);
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
+
     $courseCardDataTmpl->set('course_name', $courseData['name']);
-    $courseCardDataTmpl->set('course_data_list', $courseDatalistTmpl->parse());
+
+    foreach ($courseData['data'] as $dish => $dishData) {
+        $courseDatalistTmpl = new Template('course_data_list');
+        $courseDatalistTmpl->set('dish_name', $dishData['dish_name']);
+        $courseDatalistTmpl->set('dish_description', $dishData['dish_description']);
+        $courseDatalistTmpl->set('dish_price', $dishData['dish_price']);
+        $courseDatalistTmpl->set('discount', $dishData['discount']);
+
+        $courseCardDataTmpl->add('course_data_list', $courseDatalistTmpl->parse());
+
+    }
+
     $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
+
     $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }
 $mainTmpl->set('content', $contentTmpl->parse());
